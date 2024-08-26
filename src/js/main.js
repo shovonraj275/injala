d12b981dfee13010f4d112b03db5f945294e2722
Ezaz ahamed <73046376+shovonraj275@users.noreply.github.com>
2024-08-26 22:50:23 +0600

    ikdkkf

--- src/js/main.js
+++ src/js/main.js
@@ -13,27 +13,27 @@ document.getElementById("divi").innerHTML = newDivi;
 // document.getElementById("test").innerHTML=divi;
 // <!-- ==========  End Section ========== -->
 // <!-- ========== jala  let Start Section ========== -->
-const subDiveBarisal =["বরিশাল", "ঝালকাঠি", "পটুয়াখালী", "পিরোজপুর",  'ভোলা', "বরগুনা"];
-const subDiveChittagong =["চট্টগ্রাম","কুমিল্লা","কক্সবাজার",  "ফেনী", "ব্রাহ্মণবাড়িয়া", "নোয়াখালী", "চাঁদপুর", "লক্ষ্মীপুর", "রাঙ্গামাটি",  "খাগড়াছড়ি", "বান্দরবান"];
-const subDiveDhaka =["ঢাকা","নারায়ণগঞ্জ","গাজীপুর","নরসিংদী",  "শরীয়তপুর",  "টাঙ্গাইল", "কিশোরগঞ্জ", "মানিকগঞ্জ",  "মুন্সিগঞ্জ", "রাজবাড়ী", "মাদারীপুর", "গোপালগঞ্জ", "ফরিদপুর"];
-const subDiveKhulna=["খুলনা","যশোর", "সাতক্ষীরা", "মেহেরপুর", "নড়াইল", "চুয়াডাঙ্গা", "কুষ্টিয়া", "মাগুরা",  "বাগেরহাট", "ঝিনাইদহ"];
-const subDiveRajshahi =["রাজশাহী", "নাটোর","পাবনা","চাঁপাইনবাবগঞ্জ", "সিরাজগঞ্জ",  "বগুড়া",  "জয়পুরহাট", "নওগাঁ"];
-const subDiveRangpur =[	 "রংপুর","পঞ্চগড়", "দিনাজপুর", "লালমনিরহাট", "নীলফামারী", "গাইবান্ধা", "ঠাকুরগাঁও", "কুড়িগ্রাম"];
-const subDivieMymensingh =["ময়মনসিংহ", "শেরপুর", "জামালপুর", "নেত্রকোণা"];
-const subDivieSylhet =[ "সিলেট", "মৌলভীবাজার", "হবিগঞ্জ", "সুনামগঞ্জ"  ];
-let subDiveDhakalan  = subDiveDhaka.length;
-let subDiveBarisallan  = subDiveBarisal.length;
-let subDiveChittagonglan  = subDiveChittagong.length;
-let subDiveKhulnalan  = subDiveKhulna.length;
-let subDiveRajshahilan  = subDiveRajshahi.length;
-let subDiveRangpurlan  = subDiveRangpur.length;
-let subDivieMymensinghlan  = subDivieMymensingh.length;
-let subDivieSylhetlan  = subDivieSylhet.length;
+const subDiveBarisal =["জেলা নিবাচন করুন ", "বরিশাল", "ঝালকাঠি", "পটুয়াখালী", "পিরোজপুর",  'ভোলা', "বরগুনা"];
+const subDiveChittagong =["একটি জেলা নিবাচন করুন ","চট্টগ্রাম","কুমিল্লা","কক্সবাজার",  "ফেনী", "ব্রাহ্মণবাড়িয়া", "নোয়াখালী", "চাঁদপুর", "লক্ষ্মীপুর", "রাঙ্গামাটি",  "খাগড়াছড়ি", "বান্দরবান"];
+const subDiveDhaka =["জেলা নিবাচন করুন ","ঢাকা","নারায়ণগঞ্জ","গাজীপুর","নরসিংদী",  "শরীয়তপুর",  "টাঙ্গাইল", "কিশোরগঞ্জ", "মানিকগঞ্জ",  "মুন্সিগঞ্জ", "রাজবাড়ী", "মাদারীপুর", "গোপালগঞ্জ", "ফরিদপুর"];
+const subDiveKhulna=["জেলা নিবাচন করুন ","খুলনা","যশোর", "সাতক্ষীরা", "মেহেরপুর", "নড়াইল", "চুয়াডাঙ্গা", "কুষ্টিয়া", "মাগুরা",  "বাগেরহাট", "ঝিনাইদহ"];
+const subDiveRajshahi =["জেলা নিবাচন করুন ","রাজশাহী", "নাটোর","পাবনা","চাঁপাইনবাবগঞ্জ", "সিরাজগঞ্জ",  "বগুড়া",  "জয়পুরহাট", "নওগাঁ"];
+const subDiveRangpur =[	"জেলা নিবাচন করুন ", "রংপুর","পঞ্চগড়", "দিনাজপুর", "লালমনিরহাট", "নীলফামারী", "গাইবান্ধা", "ঠাকুরগাঁও", "কুড়িগ্রাম"];
+const subDivieMymensingh =["জেলা নিবাচন করুন ","ময়মনসিংহ", "শেরপুর", "জামালপুর", "নেত্রকোণা"];
+const subDivieSylhet =[ "জেলা নিবাচন করুন ","সিলেট", "মৌলভীবাজার", "হবিগঞ্জ", "সুনামগঞ্জ"  ];
+// let subDiveDhakalan  = subDiveDhaka.length;
+// let subDiveBarisallan  = subDiveBarisal.length;
+// let subDiveChittagonglan  = subDiveChittagong.length;
+// let subDiveKhulnalan  = subDiveKhulna.length;
+// let subDiveRajshahilan  = subDiveRajshahi.length;
+// let subDiveRangpurlan  = subDiveRangpur.length;
+// let subDivieMymensinghlan  = subDivieMymensingh.length;
+// let subDivieSylhetlan  = subDivieSylhet.length;
 
 
-x= subDiveBarisallan+subDiveChittagonglan+subDiveDhakalan+subDiveKhulnalan+subDiveRajshahilan +subDiveRangpurlan+ subDivieMymensinghlan+subDivieSylhetlan;
-y = subDiveChittagonglan;
-document.getElementById("test4").innerHTML=x;
+// x= subDiveBarisallan+subDiveChittagonglan+subDiveDhakalan+subDiveKhulnalan+subDiveRajshahilan +subDiveRangpurlan+ subDivieMymensinghlan+subDivieSylhetlan;
+// y = subDiveChittagonglan;
+// document.getElementById("test4").innerHTML=x;
 
 
 // <!-- ========== jala  let End Section ========== -->
@@ -94,6 +94,7 @@ function divis(){
   }
   else if (divival=="divi2"){
     subdivi_output=newsubDiveChittagong;
+    
   }
   else if (divival=="divi3"){
     subdivi_output=newsubDiveRajshahi;
@@ -114,16 +115,74 @@ function divis(){
     subdivi_output=newsubDivieMymensingh;
   }
 else{ subdivi=`বিভাগ নিবাচন করুন`}
+  // document.getElementById("subdivi").innerHTML=subdivi_output;
   document.getElementById("subdivi").innerHTML=subdivi_output;
 }
 // <!-- ========== onchange divi End Section ========== -->
 // <!-- ========== onchange divi Start Section ========== -->
-// function subdivi(){
-//   var subdivival =document.getElementById("subdivi").value;
-//   // console.log(divival);
-//   document.getElementById("test3").innerHTML =subdivival;
+
+
+
+
+const subdivDhaka1 =["থানা নিবাচন করুন ","তুরাগ", "উত্তরা(পশ্চিম)", "উত্তরা(পূর্ব)", "উত্তরখান", "দক্ষিণখান", "বিমানবন্দর", "খিলক্ষেত", "ভাটারা", "বাড্ডা", "গুলশান", "বনানী", "ক্যান্টনমেন্ট", "হাতিরঝিল", "তেজগাঁও শিল্পাঞ্চল", "তেজগাঁও", "শেরেবাংলা নগর", "মোহাম্মদপুর", "আদাবর", "ভাষাণটেক", "কাফরুল", "মিরপুর মডেল", "পল্লবী", "দারুচ্ছালাম", "শাহ্আলী", "রুপনগর", "রমনা মডেল", "শাহবাগ", "কলাবাগান", "নিউ মার্কেট", "ধানমন্ডি", "হাজারীবাগ", "মতিঝিল", "পল্টন মডেল", "শাহজাহানপুর", "সবুজবাগ", "মুগদা", "খিলগাঁও", "রামপুরা", "ওয়ারী", "গেন্ডারিয়া", "শ্যামপুর", "কদমতলী", "ডেমরা", "যাত্রাবাড়ী", "লালবাগ বিভাগ", "কোতয়ালী", "সুত্রাপুর", "বংশাল", "চকবাজার", "লালবাগ", "কামরাংগীরচর","সাভার", "ধামরাই", "কেরাণীগঞ্জ", "নবাবগঞ্জ", "দোহার"];
+let  newsubdivDhaka1 =[];
+subdivDhaka1.forEach(myfncrtion_subdivDhaka1);
+function myfncrtion_subdivDhaka1(subdivDhaka1,index) {
+  newsubdivDhaka1 += "<option value=subdivDhaka1" + index +">" + subdivDhaka1 + "</option>";
+}
+console.log(newsubdivDhaka1)
+document.getElementById("test5").innerHTML=newsubdivDhaka1;
+// 
+// const  =["থানা নিবাচন করুন ",];
+// let new =[];
+// .forEach(myfncrtion_);
+// function myfncrtion_(,index) {
+//   new += "<option value=subdivBarisal" + index +">" +  + "</option>";
 // }
+
+
+
+
+// <!-- ========== Start Section dhaka jaila ========== -->
+
+// <!-- ========== End Section dhaka jaila ========== -->
+// <!-- ========== Start Section foreach dhaka jaila ========== -->
+
+// <!-- ========== End Section foreach dhaka jaila ========== -->
+
+
+
+
+
+
+
+
 // <!-- ========== onchange divi End Section ========== -->
-const nn =["চট্টগ্রাম" , "রাজশাহী" , "খুলনা" , "বরিশাল" , "সিলেট" , "ঢাকা" , "রংপুর" , "ময়মনসিংহ"];
-let c=nn.sort();
-document.getElementById("sort").innerHTML=c;
+function subdivi(){
+  var thanaval =document.getElementById("subdivi").value;
+document.getElementById("test3").innerHTML=thanaval;
+
+ console.log(thanaval);
+
+ if(thanaval == "subdivDhaka1"){
+  thanaOutPut=newsubdivDhaka1;
+ }
+ else if(thanaval == "subdivDhaka2"){
+  thanaOutPut=newsubdivDhaka1;
+ }
+//  thanaOutPut=newsubdivDhaka1;
+ document.getElementById("thanaList").innerHTML=thanaOutPut;
+}
+
+
+
+
+const nn =["তুরাগ", "উত্তরা(পশ্চিম)", "উত্তরা(পূর্ব)", "উত্তরখান", "দক্ষিণখান", "বিমানবন্দর", "খিলক্ষেত", "ভাটারা", "বাড্ডা", "গুলশান", "বনানী", "ক্যান্টনমেন্ট", "হাতিরঝিল", "তেজগাঁও শিল্পাঞ্চল", "তেজগাঁও", "শেরেবাংলা নগর", "মোহাম্মদপুর", "আদাবর", "ভাষাণটেক", "কাফরুল", "মিরপুর মডেল", "পল্লবী", "দারুচ্ছালাম", "শাহ্আলী", "রুপনগর", "রমনা মডেল", "শাহবাগ", "কলাবাগান", "নিউ মার্কেট", "ধানমন্ডি", "হাজারীবাগ", "মতিঝিল", "পল্টন মডেল", "শাহজাহানপুর", "সবুজবাগ", "মুগদা", "খিলগাঁও", "রামপুরা", "ওয়ারী", "গেন্ডারিয়া", "শ্যামপুর", "কদমতলী", "ডেমরা", "যাত্রাবাড়ী", "লালবাগ বিভাগ", "কোতয়ালী", "সুত্রাপুর", "বংশাল", "চকবাজার", "লালবাগ", "কামরাংগীরচর","সাভার", "ধামরাই", "কেরাণীগঞ্জ", "নবাবগঞ্জ", "দোহার"];
+let c=nn.length;
+// document.getElementById("sort").innerHTML=c;
+// <!-- ========== Start Section dhaka jaila ========== -->
+
+// <!-- ========== End Section dhaka jaila ========== -->
+// <!-- ========== Start Section foreach dhaka jaila ========== -->
+
+// <!-- ========== End Section foreach dhaka jaila ========== -->
